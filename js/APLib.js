e571fcd989cc810f1633d1f093fb6539d92fa8b8
AlMA PRO LEADER <alma.pro.leader@gmail.com>
2018-10-16 16:58:51 +0200

    APLib Javascript structure change.

--- js/APLib.js
+++ js/APLib.js
@@ -1,136 +1,248 @@
-var frontend           = true;
+var config      = {};
+var commands    = {};
+var connections = {};
+var websockets  = {};
+var jobs        = {};
+var threads     = {};
+var functions   = {};
+
+window.APLib = {
+	Config: {
+		setURL: function(URL) {
+			config.URL = URL;
+		},
+		setVerbose: function(verbose) {
+			config.verbose = verbose;
+		},
+		setTimeout: function(timeout) {
+			config.timeout = timeout;
+		}
+	},
+	Commands: {
+		set: function(command, callback) {
+			commands[command] = callback;
+		},
+		unset: function(command) {
+			commands[command] = undefined;
+			commands          = JSON.parse(JSON.stringify(commands));
+		},
+		execute: function(command) {
+			commands[command.command](command);
+		}
+	},
+	API: {
+		connect: function(data, callback) {
+			if(data === undefined) data = JSON.stringify({command: 'refresh', type: 'all'});
+			var alreadyConnecting = false;
+			Object.keys(connections).map(function(connection){
+				if(connections[connection].data == data) alreadyConnecting = true;
+			});
+			if(alreadyConnecting) return false;
+			if(config.verbose) console.log(data);
+			var id       = APLib.Functions.call('Random String');
+			var XHR      = new XMLHttpRequest();
+			XHR.timeout  = config.timeout;
+			XHR.callback = callback;
+			XHR.data     = data;
+			XHR.id       = id;
+			XHR.onreadystatechange = function(){
+				if(this.readyState == 4 && this.status == 200 && this.responseText != '') {
+					if(config.verbose) console.log(this.responseText);
+					if(this.callback !== undefined) {
+						this.callback(this.responseText);
+					} else {
+						try {
+							var command = JSON.parse(this.responseText);
+							APLib.Commands.execute(command);
+						} catch(e) {
+							if(config.verbose) console.log(e);
+						}
+					}
+					APLib.API.remove(this.id);
+				}
+			}
+			XHR.open('POST', config.URL);
+			XHR.setRequestHeader("Content-Type", "application/json");
+			XHR.send(data);
+			connections[id] = XHR;
+			return id;
+		},
+		close: function(id) {
+			connections[id].abort();
+			APLib.API.remove(id);
+		},
+		remove: function(id) {
+			connections[id] = undefined;
+			connections     = JSON.parse(JSON.stringify(connections));
+		}
+	},
+	WebSockets: {
+		create: function(name, URL) {
+			//
+		},
+		get: function(name) {
+			//
+		},
+		connect: function(name) {
+			//
+		},
+		close: function(name) {
+			//
+		}
+	},
+	Jobs: {
+		add: function(name, call, timeout) {
+			if(APLib.Jobs.get(name)) return APLib.Jobs.get(name);
+			var f      = function(self){ self.value = setTimeout(self.call, self.timeout); }
+			f.value    = null;
+			f.call     = call;
+			f.timeout  = timeout;
+			func       = function(){ f(f) };
+		    var job    = {name, func, call: func, execute: func, run: func, start: func};
+		    jobs[name] = job;
+			func();
+		    return job;
+		},
+		remove: function(name) {
+		    if(jobs[name].value != null) clearTimeout(jobs[name].value);
+			jobs[name] = undefined;
+			jobs       = JSON.parse(JSON.stringify(jobs));
+		},
+		get: function(name) {
+		    if(jobs[name] !== undefined) return jobs[name];
+		    return false;
+		},
+		reset: function() {
+			Object.keys(jobs).map(function(job){
+				APLib.Jobs.remove(job);
+			})
+		    jobs = {};
+		},
+		count: function() {
+		    return Object.keys(jobs).length;
+		}
+	},
+	Threads: {
+		add: function(name, call, interval) {
+		    if(APLib.Threads.get(name)) return APLib.Threads.get(name);
+		    var func      = function(self){ APLib.Threads.start(self.tname) };
+			func.tname    = name;
+		    var startFunc = function(){ func(func) };
+		    var thread    = {name, interval, callFunc: call, running: false, call: startFunc, execute: startFunc, run: startFunc, start: startFunc};
+		    threads[name] = thread;
+		    return thread;
+		},
+		start: function(name) {
+		    var thread = APLib.Threads.get(name);
+		    if(thread.running) return;
+		    APLib.Threads.setInterval(name, setInterval(thread.callFunc, thread.interval));
+		    APLib.Threads.setState(name, true);
+		},
+		stop: function(name) {
+		    var thread = APLib.Threads.get(name);
+		    if(!thread.running) return;
+		    clearInterval(thread.value);
+		    APLib.Threads.setState(name, false);
+		},
+		remove: function(name) {
+		    APLib.Threads.stop(name);
+		    threads[name] = undefined;
+			threads       = JSON.parse(JSON.stringify(threads));
+		},
+		get: function(name) {
+		    if(threads[name] !== undefined) return threads[name];
+		    return false;
+		},
+		setInterval: function(name, interval) {
+		    threads[name].value = interval;
+		},
+		setState: function(name, running) {
+			threads[name].running = running;
+		},
+		reset: function() {
+			Object.keys(threads).map(function(thread){
+				APLib.Threads.remove(thread);
+			});
+		    threads = {};
+		},
+		count: function() {
+		    return Object.keys(threads).length;
+		}
+	},
+	Functions: {
+		create: function(name, call) {
+			functions[name] = call;
+		},
+		call: function(name, args) {
+			return functions[name](args);
+		}
+	},
+	Security: {
+		check: function(data) {
+			//
+		}
+	}
+}
+
+APLib.Config.setURL(location.href);
+APLib.Config.setVerbose(false);
+APLib.Config.setTimeout(0);
+APLib.Commands.set('APLib', function(command) {
+	if(command.verbose) APLib.Config.setVerbose(command.verbose);
+	if(command.timeout) APLib.Config.setTimeout(command.timeout);
+	if(command.alert) APLib.Commands.execute({command: 'notify', ...command.alert});
+	if(command.security) APLib.Commands.execute({command: 'security', ...command.security});
+})
+APLib.Commands.set('notify', function(command) {
+	if(command.placement === undefined || command.placement == null) command.placement = {from: 'top', align: 'center'};
+	var alertConfigs = {
+		title: "<strong>" + command.title + "</strong><BR>",
+		message: command.message
+	};
+	var alertOptions = {
+		animate: {
+			enter: 'animated fadeInDown',
+			exit: 'animated fadeOutUp'
+		},
+		placement: command.placement,
+		offset: 10,
+		mouse_over: 'pause'
+	};
+	switch(command.type){
+		case 'error':
+			alertOptions['type'] = 'danger';
+			break;
+		case 'success':
+			alertOptions['type'] = 'success';
+			break;
+		case 'warning':
+			alertOptions['type'] = 'warning';
+			alertConfigs['icon'] = 'glyphicon glyphicon-warning-sign';
+			break;
+	}
+	$.notify(alertConfigs, alertOptions);
+})
+APLib.Commands.set('security', function(command) {
+	switch(command.action){
+		case 'captcha':
+			// TODO: Impelment captcha
+			break;
+		case 'warning':
+			APLib.Commands.execute({command: 'notify', title: 'Security warning', message: command.message, placement: undefined, type: 'warning'});
+			break;
+	}
+})
+APLib.Functions.create('Random String', function(length) {
+	length     = (length === undefined) ? 5 : length;
+	var string = "";
+	var chars  = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for (var i = 0; i < length; i++) string += chars.charAt(Math.floor(Math.random() * chars.length));
+	return string;
+});
+
 var shiftDown          = false;
 var ctrlDown           = false;
 var altDown            = false;
-var runner             = null;
-var extraRunner        = null;
-var extraCommands      = [];
-var verbose            = false;
-var lastAlertT         = '';
-var lastAlertM         = '';
-var wait               = false;
-var lastInterval       = 0;
-window.APLibBackendURL = location.href;
-
-function APLib(interval){
-	if(interval === undefined) interval = 1000;
-	if(lastInterval == interval) return;
-	if(runner != null) clearInterval(runner);
-	runner = setInterval(
-		function(){
-			if(!wait) BackEnd();
-		},
-		interval
-	);
-	lastInterval = interval;
-}
-function BackEnd(data, callback){
-	wait = true;
-	var xmlHTTP =  new XMLHttpRequest();
-	xmlHTTP.onreadystatechange = function(){
-		if(xmlHTTP.readyState == 4) wait = false;
-		if(xmlHTTP.readyState == 4 && xmlHTTP.status == 200 && xmlHTTP.responseText != ''){
-			if(verbose) console.log(xmlHTTP.responseText);
-			if(callback !== undefined){
-				callback(xmlHTTP.responseText);
-				return;
-			}
-			try{
-				var data = JSON.parse(xmlHTTP.responseText);
-			}catch(e){
-				return;
-			}
-			FrontEnd(data);
-		}
-	}
-	xmlHTTP.open('POST', APLibBackendURL);
-	if(data === undefined){
-		data = JSON.stringify(
-			{
-				'command' : 'refresh',
-				'type'    : 'all'
-			}
-		);
-		if(!frontend) data = JSON.stringify({'command' : 'disabled'});
-	}
-	if(verbose) console.log(data);
-	xmlHTTP.setRequestHeader(
-		"Content-Type",
-		"application/json"
-	);
-	xmlHTTP.send(data);
-}
-function FrontEnd(command){
-	if(command.verbose){
-		if(!verbose){
-			console.log('Verbosity has been enabled');
-			verbose = true;
-		}
-	}else{
-		if(verbose){
-			console.log('Verbosity has been disabled');
-			verbose = false;
-		}
-	}
-	if(typeof(command.security) != 'undefined'){
-		switch(command.security){
-			case 'captcha':
-				// TODO: Impelment captcha
-				break;
-			case 'warning':
-				notify('Security warning', command.security.message, null, 'warning');
-				break;
-		}
-	}
-	if(typeof(command.interval) != 'undefined'){
-		APLib(command.interval);
-	}
-	switch(command.command){
-		case 'update':
-			if($(command.selector).html() != command.html) $(command.selector).html(command.html);
-			break;
-		case 'alert':
-			var placement = null;
-			if(typeof(command.placement) != 'undefined') placement = command.placement;
-			notify(command.title, command.message, placement, command.type);
-			break;
-		case 'disable':
-			if(frontend){
-				console.log("Front-End has been disabled: " + command.reason);
-				var placement = null;
-				if(typeof(command.placement) != 'undefined') placement = command.placement;
-				notify("Front-End has been disabled", command.reason, placement, 'warning');
-				frontend=false;
-			}
-			break;
-		case 'enable':
-			if(!frontend){
-				console.log("Front-End has been enabled");
-				var placement = null;
-				if(typeof(command.placement) != 'undefined') placement = command.placement;
-				notify("Front-End has been enabled", '', placement, 'success');
-				frontend=true;
-			}
-			break;
-		case 'terminate':
-			clearInterval(runner);
-			if(extraRunner != null) clearInterval(extraRunner);
-			frontend = false;
-			console.log('Front-End has been terminated: ' + command.reason);
-			var placement = null;
-			if(typeof(command.placement) != 'undefined') placement = command.placement;
-			notify("Front-End has been terminated", command.reason, placement, 'error');
-			break;
-		default:
-			for(var i = 0; i < extraCommands.length; i++){
-				if(extraCommands[i][0] == command.command){
-					var exe = new Function('command', extraCommands[i][1])
-					exe(command);
-				}
-			}
-			break;
-	}
-}
 function checkDown(e){
 	var code = (e.keyCode ? e.keyCode : e.which);
 	switch(code){
@@ -172,38 +284,6 @@ function scrollToView(element){
 	var scrollHeight = Math.max(element.scrollHeight, element.clientHeight);
 	element.scrollTop = scrollHeight - element.clientHeight;
 }
-function notify(title, message, placement, type){
-	if(lastAlertT == title && lastAlertM == message) return;
-	lastAlertT = title;
-	lastAlertM = message;
-	if(placement === undefined || placement == null) placement = {from: 'bottom', align: 'left'};
-	var alertConfigs = {
-		title: "<strong>" + title + "</strong><BR>",
-		message: message
-	};
-	var alertOptions = {
-		animate: {
-			enter: 'animated fadeInDown',
-			exit: 'animated fadeOutUp'
-		},
-		placement: placement,
-		offset: 10,
-		mouse_over: 'pause'
-	};
-	switch(type){
-		case 'error':
-			alertOptions['type'] = 'danger';
-			break;
-		case 'success':
-			alertOptions['type'] = 'success';
-			break;
-		case 'warning':
-			alertOptions['type'] = 'warning';
-			alertConfigs['icon'] = 'glyphicon glyphicon-warning-sign';
-			break;
-	}
-	$.notify(alertConfigs,alertOptions);
-}
 /************/
 /*  Checks  */
 /************/
@@ -216,14 +296,12 @@ function notify(title, message, placement, type){
 // Return:
 //        void
 window.onlyNumbers = function(e){
-  if ($.inArray(e.keyCode, [46, 8, 9, 27, 13]) !== -1 ||
-      (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) ||
-      (e.keyCode >= 35 && e.keyCode <= 40)) {
-           return;
-  }
-  if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
-      e.preventDefault();
-  }
+	if ($.inArray(e.keyCode, [46, 8, 9, 27, 13]) !== -1 || (e.keyCode === 65 && (e.ctrlKey === true || e.metaKey === true)) || (e.keyCode >= 35 && e.keyCode <= 40)) {
+		return;
+	}
+	if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+		e.preventDefault();
+	}
 }
 
 // Usage:
